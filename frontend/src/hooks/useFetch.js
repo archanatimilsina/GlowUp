0ec511210762313bd3c197f1920450f77f075c26
archanatimilsina <archanatimilsina88@gmail.com>
2026-06-23 14:53:33 +0545

    project Update

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-const API_BASE = import.meta.env.VITE_API_URL;
+// const API_BASE = import.meta.env.VITE_API_URL;
 
 const useFetch =(url)=>
 {
@@ -15,7 +15,7 @@ async (options)=>
     setLoading(true)
     setError(null)
     try {
-          const response = await fetch(`${API_BASE}/${url}`, options)
+          const response = await fetch(url, options)
         const result = await response.json()
         if(!response.ok)
         {
